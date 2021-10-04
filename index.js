c92f45bab531b1b5e19da6c3df0180f5e6cd1c97
youthfulanger <prorock-666@inbox.ru>
2021-10-04 12:26:11 +0900

    nvm

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ for (let x = 0; x < columns; x++) {
 }
 
 const draw = () => {
-  context.fillStyle = 'rgba(0, 0, 0, 0.009)'
+  context.fillStyle = 'rgba(0, 0, 0, 0.05)'
   context.fillRect(0, 0, canvas.width, canvas.height)
   context.fillStyle = '#0F0'
   context.font = fontSize + 'px monospace'
