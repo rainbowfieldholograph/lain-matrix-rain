4df3ef1c6b03e407416c71f04bf9c9f4dff5a45a
cherry <82304659+rainbowfieldholograph@users.noreply.github.com>
2022-08-20 01:21:34 +0900

    javascript code refactor

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-const canvas = document.getElementById('Matrix')
-const context = canvas.getContext('2d')
+const canvas = document.getElementById('Matrix');
+const context = canvas.getContext('2d');
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
+const LATIN = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const NUMBERS = '0123456789';
+const CHARS = LATIN + NUMBERS;
+const FONT_SIZE = 16;
 
-const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const nums = '0123456789'
+let rainDrops = [];
+let columns = 0;
 
-const alphabet = latin + nums
-
-const fontSize = 16
-let columns = canvas.width / fontSize
-
-const rainDrops = []
-
-for (let x = 0; x < columns; x++) {
-  rainDrops[x] = 1
-}
+const initMatrix = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  columns = canvas.width / FONT_SIZE;
+  rainDrops = Array.from({ length: columns }, () => 1);
+};
 
 const draw = () => {
-  context.fillStyle = 'rgba(0, 0, 0, 0.05)'
-  context.fillRect(0, 0, canvas.width, canvas.height)
-  context.fillStyle = '#0F0'
-  context.font = fontSize + 'px monospace'
-  for (let i = 0; i < rainDrops.length; i++) {
-    const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length))
-    context.fillText(text, i * fontSize, rainDrops[i] * fontSize)
-    if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
-      rainDrops[i] = 0
+  context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = '#0F0';
+  context.fontSize = `${FONT_SIZE}px`;
+
+  for (const prop in rainDrops) {
+    const text = CHARS.charAt(Math.floor(Math.random() * CHARS.length));
+    context.fillText(text, prop * FONT_SIZE, rainDrops[prop] * FONT_SIZE);
+    if (rainDrops[prop] * FONT_SIZE > canvas.height && Math.random() > 0.975) {
+      rainDrops[prop] = 0;
     }
-    rainDrops[i]++
+    rainDrops[prop]++;
   }
-}
+};
 
-setInterval(draw, 30)
+// ===
 
-window.addEventListener('resize', function () {
-  console.log('resize')
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-  columns = canvas.width / fontSize
-  for (let x = 0; x < columns; x++) {
-    rainDrops[x] = 1
-  }
-})
+initMatrix();
+
+setInterval(draw, 30);
+
+window.addEventListener('resize', () => {
+  initMatrix();
+});
